d24157dce3b4fb72239d63cd8f8c12010ad2f361
Helder Neto Alves <hellder.alves@yahoo.com.br>
2020-10-08 11:53:34 -0300

    ok now

--- callback.js
+++ callback.js
@@ -1,3 +1,28 @@
+//promisses
+
+const doSomeThingPromise = () => 
+new Promise((resolve, reject) => {
+    setTimeout(function(){
+        //did something
+        resolve('first data');
+    }, 1500);
+});
+
+const doOtherThingPromise = () => 
+new Promise((resolve, reject) => {
+    setTimeout(function(){
+        //did other thing
+        resolve('second data');
+    }, 1000);
+});
+
+Promise.race([doSomeThingPromise(), doOtherThingPromise()]).then(data => {
+    console.log(data);
+});
+
+
+
+// callbacks
 function doSomething(callback) {
     setTimeout(function(){
         callback('first data');
@@ -11,16 +36,27 @@ function doOtherThing(callback){
 }
 
 function doAll(){
+    try{
     doSomething(function(data){
         var processedData = data.split('');
+        try{
 
         doOtherThing(function(data2){
             var processedData2 = data2.split('');
-            
+            try{
             setTimeout(function(){
                 console.log(processedData, processedData2);
             },1000);
+        }catch(err) {
+            //handle error
+        }
         });
+    } catch(err) {
+        //handle error
+    }
     });
+}catch(err) {
+    //handle error
+}
 }
 doAll();
